b121c1d688771dfbbdbc7d46f834f320d80223d5
Francisco Cisneros <janci.trabajo@gmail.com>
2025-04-08 00:17:19 +0200

    errores al hacer commit arreglados
    se crearon errores a la hora de hacer commit
    se duplicaron fracciones de coidgo

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -192,71 +192,7 @@ function Login() {
             </form>
           </div>
         </div>
-          <div className='contenedorRegistro'>
-            <form onSubmit={handleRegisterSubmit}>
-              <div>
-                <p>Nombre</p>
-                <input
-                  className='inputUsuario'
-                  type="text"
-                  name="nombre"
-                  value={registerData.nombre}
-                  onChange={handleRegisterChange}
-                  placeholder="Nombre"
-                  required
-                />
-              </div>
-                <div>
-                  <p>Apellido</p>
-                  <input
-                    className='inputUsuario'
-                    type="text"
-                    name="apellido"
-                    value={registerData.apellido}
-                    onChange={handleRegisterChange}
-                    placeholder="Apellido"
-                    required
-                  />
-                </div>
-                <div>
-                  <p>Correo electrónico</p>
-                  <input
-                    className='inputUsuario'
-                    type="email"
-                    name="email"
-                    value={registerData.email}
-                    onChange={handleRegisterChange}
-                    placeholder="correo@ejemplo.com"
-                    required
-                  />
-                </div>
-                <div>
-                  <p>Contraseña</p>
-                  <input
-                    className='inputUsuario'
-                    type="password"
-                    name="contraseña"
-                    value={registerData.contraseña}
-                    onChange={handleRegisterChange}
-                    placeholder="**********"
-                    required
-                  />
-                </div>
-                <div>
-                  <p>Fecha de nacimiento</p>
-                  <input
-                    className='inputUsuario'
-                    type="date"
-                    name="fecha_nacimiento"
-                    value={registerData.fecha_nacimiento}
-                    onChange={handleRegisterChange}
-                    required
-                  />
-                </div>
-                <button type="submit">Crear cuenta</button>
-            </form>
-          </div>
-        </div>
+      </div>
     </div>
   );
 }
